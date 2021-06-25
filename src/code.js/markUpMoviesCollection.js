c0e11a881d099daa1ea3c313268014d40691b853
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-06-25 16:20:07 +0200

    фиксит myLibrary удаляет console log с прорисовки списков

--- src/code.js/markUpMoviesCollection.js
+++ src/code.js/markUpMoviesCollection.js
@@ -16,20 +16,15 @@ export default class markUpMoviesCollection {
 
     fetchPersonsCollectionMovies(e) {
         const moviesCollection = e;
-        console.log('moviesCollection', moviesCollection)
 
         moviesCollection.forEach(movie_id => {
-            console.log('id', movie_id)
             fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=44d74a10460e9a32f8546bed31d47780&language=en-US`)
                 .then(res => {
-                    console.log(res)
                     if (res.ok) {
-                        console.log('res.json ', res)
                         return res.json()
                     }
                 })
                 .then(res => {
-                    console.log('markUp ', res)
                     const markUp = collectionMovies([res]);
                     this.refs.container.insertAdjacentHTML('beforeend', markUp);
                 })
