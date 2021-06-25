efb3bc4ab23330f38a673d2bc6c408363f1abfec
AlexYurchenk <75500247+AlexYurchenk@users.noreply.github.com>
2021-06-26 00:23:36 +0300

    Update pagination.js

--- src/code.js/pagination.js
+++ src/code.js/pagination.js
@@ -14,7 +14,6 @@ export default class PaginationService {
         fetch(`${BASE__URL}movie?api_key=${API__KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${n}&with_watch_monetization_types=flatrate`)
         .then(r => r.json())
         .then( films => {
-            console.log(films)
             return films
         })
         .then( ({results} ) =>{
@@ -60,9 +59,7 @@ refs.btnList.addEventListener('click', e => {
     if(e.target.nodeName!== 'BUTTON'){
         return
     }
-    
-    console.log(e.target.nodeName,e.currentTarget)
-    
+        
     n = e.target.textContent -0;
     btnCreate()
 
